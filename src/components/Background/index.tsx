696e2490e2fb727bd16b7dd799b5fa7132c79344
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-09-08 10:02:22 -0300

    Novas alterações

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -3,16 +3,30 @@ import style from "./style.module.scss";
 export function Background() {
   return (
     <main className={style.main}>
-      <section>
+      <section className={style.contentTop}>
         <div className={style.navegation}>
-          <img src="" alt="" />
-
+          <img src="../../images/logo.svg" alt="" />
           <nav>
             <a href="#">home</a>
             <a href="#">shop</a>
             <a href="#">about</a>
             <a href="#">contact</a>
           </nav>
+        </div>
+        <div className={style.describeContent}>
+          <div className={style.discover}>
+            <h1>Discover innovative ways to decorate</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+              voluptate quisquam ea, tempora amet laboriosam voluptatibus a modi
+              vitae repudiandae pariatur. Tempora molestias consectetur fugit
+              corporis ipsum quia incidunt voluptates!
+            </p>
+          </div>
+          <div>
+            <button>1</button>
+            <button>2</button>
+          </div>
         </div>
       </section>
     </main>
