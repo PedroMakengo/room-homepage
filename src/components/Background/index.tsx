15e2e427f4d714229ad862e1f3ff3e94166607e1
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-09-08 09:35:12 -0300

    Melhorando

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,7 +1,20 @@
+import style from "./style.module.scss";
+
 export function Background() {
   return (
-    <div>
-      <h1>Olá Mundo - Funcionando</h1>
-    </div>
+    <main className={style.main}>
+      <section>
+        <div className={style.navegation}>
+          <img src="" alt="" />
+
+          <nav>
+            <a href="#">home</a>
+            <a href="#">shop</a>
+            <a href="#">about</a>
+            <a href="#">contact</a>
+          </nav>
+        </div>
+      </section>
+    </main>
   );
 }
