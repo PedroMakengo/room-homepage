10d9614a5c4824b0f1cbf2d8b35d741aac0d4499
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-09-08 14:53:34 -0300

    Adicionando react-icons

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -17,11 +17,13 @@ export function Background() {
           <div className={style.discover}>
             <h1>Discover innovative ways to decorate</h1>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-              voluptate quisquam ea, tempora amet laboriosam voluptatibus a modi
-              vitae repudiandae pariatur. Tempora molestias consectetur fugit
-              corporis ipsum quia incidunt voluptates!
+              We provide unmatched quality, comfort, and style for property
+              owners across the country. Our experts combine form and function
+              in bringing your vision to life. Create a room in your own style
+              with our collection and make your property a reflection of you and
+              what you love.
             </p>
+            <a href="#">SHOP NOW </a>
           </div>
           <div className={style.buttons}>
             <button className={style.active}>1</button>
