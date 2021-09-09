7562e0cf168a7f792b8a22eb9d791c891cff6c9d
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-09-09 07:23:06 -0300

    Adicionando o componente discover

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,3 +1,4 @@
+import { Discover } from "../Discover";
 import { Nav } from "../Nav";
 import style from "./style.module.scss";
 
@@ -7,19 +8,7 @@ export function Background() {
       <section className={style.contentTop}>
         <Nav />
         <div className={style.describeContent}>
-          <div className={style.discover}>
-            <h1>Discover innovative ways to decorate</h1>
-            <p>
-              We provide unmatched quality, comfort, and style for property
-              owners across the country. Our experts combine form and function
-              in bringing your vision to life. Create a room in your own style
-              with our collection and make your property a reflection of you and
-              what you love.
-            </p>
-            <a href="#">
-              SHOP NOW <img src="../../images/icon-arrow.svg" alt="" />{" "}
-            </a>
-          </div>
+          <Discover />
           <div className={style.buttons}>
             <button className={style.active}>
               <img src="../../images/icon-angle-left.svg" alt="" />
